01680c8eb7b4fc5aa5d20a9e6223788677e1dd36
Vasily Kalmykov <vasily.sergeevich.k@gmail.com>
2024-01-16 08:18:45 +0400

    build: init an app

---
+++ src/script.ts
@@ -0,0 +1,5 @@
+import * as PIXI from 'pixi.js';
+import * as TWEEN from '@tweenjs/tween.js';
+
+console.log(PIXI);
+console.log(TWEEN);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/script.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
